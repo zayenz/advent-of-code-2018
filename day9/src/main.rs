e9995d17ca5eb84fa1f63507a42541b3cf920b06
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2018-12-10 08:27:49 +0100

    Solve Day 0, part 1

---
+++ day9/src/main.rs
@@ -0,0 +1,142 @@
+#![allow(
+    dead_code,
+    unused_imports,
+    clippy::needless_range_loop,
+    clippy::ptr_arg,
+    clippy::char_lit_as_u8
+)]
+
+use std::char;
+use std::cmp::{max, min};
+use std::fmt;
+use std::io::BufRead;
+use std::iter::*;
+use std::ops::*;
+use std::str;
+use std::str::FromStr;
+use std::{io, process};
+
+use failure::bail;
+use failure::err_msg;
+use failure::Error;
+use hashbrown::{HashMap, HashSet};
+use rayon::prelude::*;
+use stats::Frequencies;
+use structopt::StructOpt;
+use strum_macros::EnumString;
+use tap::{TapOps, TapOptionOps, TapResultOps};
+
+use aoc2018::input::*;
+use aoc2018::matrix::*;
+use std::collections::BTreeSet;
+
+type Input = (usize, usize);
+type Output = usize;
+
+fn read_input() -> Result<Input, Error> {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let numbers = get_numbers::<usize>(&line)?;
+        if numbers.len() == 2 {
+            return Ok((numbers[0], numbers[1]));
+        }
+    }
+
+    bail!("Did not find any integers in input")
+}
+
+fn step(pos: usize, steps: i32, marbles: usize) -> usize {
+    if steps >= 0 {
+        (pos + (steps as usize)) % marbles
+    } else {
+        let steps = -steps as usize;
+        //        println!("pos {} steps {} marbles {}", pos, steps, marbles);
+        if pos < steps {
+            marbles - (steps - pos)
+        } else {
+            pos - steps
+        }
+    }
+}
+
+fn play(players: usize, max_score: usize) -> Vec<usize> {
+    let mut scores = vec![0; players];
+    let mut marbles = vec![0];
+    let mut pos = 0;
+    let mut player = 0;
+    for marble in 1..=max_score {
+        //        print!("PLayer {} and marble {} in ", player, marble);
+        //        for i in 0..marbles.len() {
+        //            if i == pos {
+        //                print!(">");
+        //            }
+        //            print!("{} ", marbles[i])
+        //        }
+        //        println!();
+
+        if marble % 23 == 0 {
+            scores[player] += marble;
+            pos = step(pos, -7, marbles.len());
+            scores[player] += marbles[pos];
+            marbles.remove(pos);
+        } else {
+            pos = step(pos, 1, marbles.len());
+            marbles.insert(pos + 1, marble);
+            pos += 1;
+        }
+        player = (player + 1) % players;
+    }
+
+    scores
+}
+
+fn solve1(input: &mut Input) -> Result<Output, Error> {
+    let (players, max_score) = *input;
+    let scores = play(players, max_score);
+    //    println!("{:?}", scores);
+    scores
+        .into_iter()
+        .max()
+        .ok_or_else(|| err_msg("No scores?"))
+}
+
+fn solve2(input: &mut Input) -> Result<Output, Error> {
+    Ok(2)
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "day6")]
+struct Opt {
+    /// Part to solve, either 1 or 2
+    #[structopt(short = "-p", long = "--part", default_value = "1")]
+    part: u8,
+}
+
+fn run() -> Result<(), Error> {
+    let mut input = read_input()?;
+
+    let options: Opt = Opt::from_args();
+
+    let output = if options.part == 1 {
+        solve1(&mut input)?
+    } else {
+        solve2(&mut input)?
+    };
+
+    println!("{}", output);
+    Ok(())
+}
+
+fn main() {
+    match run() {
+        Ok(()) => process::exit(0),
+        Err(error) => {
+            eprintln!("Error while solving problem: {}", error);
+            for cause in error.iter_causes() {
+                eprintln!("{}", cause)
+            }
+            process::exit(1)
+        }
+    }
+}
---
+++ day9/tests/samples.rs
@@ -0,0 +1,84 @@
+use assert_cli;
+
+#[test]
+fn sample1_1() {
+    assert_cli::Assert::main_binary()
+        .stdin("9 players; last marble is worth 25 points")
+        .stdout()
+        .is("32")
+        .unwrap();
+}
+
+#[test]
+fn sample1_2() {
+    assert_cli::Assert::main_binary()
+        .stdin("10 players; last marble is worth 1618 points")
+        .stdout()
+        .is("8317")
+        .unwrap();
+}
+
+#[test]
+fn sample1_3() {
+    assert_cli::Assert::main_binary()
+        .stdin("13 players; last marble is worth 7999 points")
+        .stdout()
+        .is("146373")
+        .unwrap();
+}
+
+#[test]
+fn sample1_4() {
+    assert_cli::Assert::main_binary()
+        .stdin("17 players; last marble is worth 1104 points")
+        .stdout()
+        .is("2764")
+        .unwrap();
+}
+
+#[test]
+fn sample1_5() {
+    assert_cli::Assert::main_binary()
+        .stdin("21 players; last marble is worth 6111 points")
+        .stdout()
+        .is("54718")
+        .unwrap();
+}
+
+#[test]
+fn sample1_6() {
+    assert_cli::Assert::main_binary()
+        .stdin("30 players; last marble is worth 5807 points")
+        .stdout()
+        .is("37305")
+        .unwrap();
+}
+
+#[test]
+fn puzzle1() {
+    assert_cli::Assert::main_binary()
+        .stdin(include_str!("../data/puzzle1.in"))
+        .stdout()
+        .is("404611")
+        .unwrap();
+}
+
+#[test]
+fn sample2_1() {
+    assert_cli::Assert::main_binary()
+        .with_args(&["-p", "2"])
+        .stdin("9 players; last marble is worth 25 points")
+        .stdout()
+        .is("2")
+        .unwrap();
+}
+
+#[test]
+fn puzzle2() {
+    assert_cli::Assert::main_binary()
+        .with_args(&["-p", "2"])
+        .stdin(include_str!("../data/puzzle1.in"))
+        .stdout()
+        .is("2")
+        .unwrap();
+}
