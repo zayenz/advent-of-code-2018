e91b66a721bbb5763c2053a2e1710d4d0e8840df
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2018-12-18 10:14:45 +0100

    Solve Day 18, part 1

---
+++ day18/src/main.rs
@@ -0,0 +1,212 @@
+#![allow(
+    dead_code,
+    unused_imports,
+    clippy::needless_range_loop,
+    clippy::ptr_arg,
+    clippy::char_lit_as_u8
+)]
+
+use std::char;
+use std::cmp::{max, min};
+use std::fmt;
+use std::io::BufRead;
+use std::iter::*;
+use std::ops::*;
+use std::str;
+use std::str::FromStr;
+use std::{io, process};
+
+use failure::bail;
+use failure::err_msg;
+use failure::Error;
+use hashbrown::{HashMap, HashSet};
+use itertools::Itertools;
+use rayon::prelude::*;
+use stats::Frequencies;
+use structopt::StructOpt;
+use strum_macros::EnumString;
+use tap::{TapOps, TapOptionOps, TapResultOps};
+
+use aoc2018::dense_grid::Grid;
+use aoc2018::dense_grid::*;
+use aoc2018::input::*;
+use aoc2018::matrix::*;
+use aoc2018::position::*;
+use std::collections::BTreeSet;
+use std::fmt::Display;
+use std::fmt::Formatter;
+
+type Input = Grid<Tile>;
+type Output = String;
+
+#[derive(EnumString, Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+enum Tile {
+    Empty,
+    Open,
+    Trees,
+    Lumberyard,
+}
+
+use crate::Tile::*;
+
+impl Default for Tile {
+    fn default() -> Self {
+        Empty
+    }
+}
+
+impl Display for Tile {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "{}",
+            match *self {
+                Empty => ' ',
+                Open => '.',
+                Trees => '|',
+                Lumberyard => '#',
+            }
+        )
+    }
+}
+
+fn read_input() -> Result<Input, Error> {
+    let stdin = io::stdin();
+    let lines = stdin
+        .lock()
+        .lines()
+        .map(|s| s.unwrap().trim().to_owned())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.chars().collect_vec())
+        .collect_vec();
+    let height = lines.len();
+    let width = lines[0].len();
+    assert!(lines.iter().map(|v| v.len()).all_equal());
+    let mut grid = Grid::from_origo(width + 2, height + 2);
+
+    for x in 0..width {
+        for y in 0..height {
+            let pos = (x + 1, y + 1);
+            match lines[y][x] {
+                '.' => {
+                    grid[pos] = Open;
+                }
+                '#' => {
+                    grid[pos] = Lumberyard;
+                }
+                '|' => {
+                    grid[pos] = Trees;
+                }
+                ch => {
+                    bail!(format!(
+                        "Unrecognized input charachter '{}' at {},{}",
+                        ch, x, y
+                    ));
+                }
+            }
+        }
+    }
+
+    //    println!("Read initial map:");
+    //    println!("{}", grid);
+
+    Ok(grid)
+}
+
+fn checksum(grid: &Grid<Tile>) -> u64 {
+    let frequencies = grid.values.iter().cloned().collect::<Frequencies<Tile>>();
+    frequencies.count(&Trees) * frequencies.count(&Lumberyard)
+}
+
+fn solve1(input: &mut Input) -> Result<Output, Error> {
+    let mut current = input.clone();
+
+    //    println!("Start:\n{}", current);
+
+    for _ in 0..10 {
+        let mut next = current.clone(); //Grid::from_origo(current.width, current.height);
+        for x in 1..current.width - 1 {
+            for y in 1..current.height - 1 {
+                let pos: Position = (x, y).into();
+                let tile = current[pos];
+                let neighbours = connect8(pos)
+                    .map(|n| current[n])
+                    .collect::<Frequencies<Tile>>();
+                next[pos] = match tile {
+                    Empty => Empty,
+                    Open => {
+                        if neighbours.count(&Trees) >= 3 {
+                            Trees
+                        } else {
+                            Open
+                        }
+                    }
+                    Trees => {
+                        if neighbours.count(&Lumberyard) >= 3 {
+                            Lumberyard
+                        } else {
+                            Trees
+                        }
+                    }
+                    Lumberyard => {
+                        if neighbours.count(&Lumberyard) >= 1 && neighbours.count(&Trees) >= 1 {
+                            Lumberyard
+                        } else {
+                            Open
+                        }
+                    }
+                }
+            }
+        }
+        //        println!("After iteration {}:\n{}", i, next);
+
+        current = next;
+    }
+
+    Ok(format!("{}", checksum(&current)))
+}
+
+fn solve2(_input: &mut Input) -> Result<Output, Error> {
+    Ok("2".to_owned())
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "day6")]
+struct Opt {
+    /// Part to solve, either 1 or 2
+    #[structopt(short = "-p", long = "--part", default_value = "1")]
+    part: u8,
+}
+
+fn run() -> Result<(), Error> {
+    let mut input = read_input()?;
+
+    let options: Opt = Opt::from_args();
+
+    let output = if options.part == 1 {
+        solve1(&mut input)?
+    } else {
+        solve2(&mut input)?
+    };
+
+    println!("{}", output);
+    Ok(())
+}
+
+fn main() {
+    match run() {
+        Ok(()) => process::exit(0),
+        Err(error) => {
+            eprintln!("Error while solving problem: {}", error);
+            for cause in error.iter_causes() {
+                eprintln!("{}", cause)
+            }
+            process::exit(1)
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+}
---
+++ day18/tests/samples.rs
@@ -0,0 +1,50 @@
+use assert_cli;
+
+#[test]
+fn sample1_1() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            ".#.#...|#.
+.....#|##|
+.|..|...#.
+..|#.....#
+#.#|||#|#|
+...#.||...
+.|....|...
+||...#|.#|
+|.||||..|.
+...#.|..|.",
+        )
+        .stdout()
+        .is("1147")
+        .unwrap();
+}
+
+#[test]
+fn puzzle1() {
+    assert_cli::Assert::main_binary()
+        .stdin(include_str!("../data/puzzle1.in"))
+        .stdout()
+        .is("483840")
+        .unwrap();
+}
+
+#[test]
+fn sample2_1() {
+    assert_cli::Assert::main_binary()
+        .with_args(&["-p", "2"])
+        .stdin("51589")
+        .stdout()
+        .is("2")
+        .unwrap();
+}
+
+#[test]
+fn puzzle2() {
+    assert_cli::Assert::main_binary()
+        .with_args(&["-p", "2"])
+        .stdin(include_str!("../data/puzzle1.in"))
+        .stdout()
+        .is("2")
+        .unwrap();
+}
